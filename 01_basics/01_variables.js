1509a288bc60146b8e51078484053482419fe10d
adarshojhacseai24-dot <adarsh.ojhacseai24@ssipmt.com>
2026-09-17 20:40:00 +0000

    variables, datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1335
+var accountEmail = "adarshojha@google.com"
+let accountPassword = "12345"
+accountCity = "Raigarh"
+
+// accountId = 2 // not allowed 
+
+accountEmail = "ao@google.com"    
+accountPassword = "54321"
+accountCity = "Raipur"
+ 
+console.log (accountId)
+
+console.table ([accountEmail, accountPassword, accountCity])
